9ba7edffa6908e430a978143046162c37c57ffac
scottmotion <scott@scottmotion.com>
2023-08-31 13:35:00 -0700

    flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { createContext, useState, useEffect, MouseEvent } from 'react';
 export const ThemeContext = createContext('light');
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    }
+  }, []);
 
   useEffect(() => {
     var htmlElement = document.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
@@ -14,24 +20,27 @@ function App() {
     }
   }, [theme]);
 
-  function toggleTheme(e: MouseEvent) {
+  function toggleTheme(e: MouseEvent<HTMLButtonElement>) {
     e.stopPropagation();
     if (theme === 'dark') {
       setTheme('light');
     } else if (theme === 'light') {
       setTheme('dark');
     }
+    console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
   }
 
   return (
     <ThemeContext.Provider value={theme}>
       <main className="flex flex-col items-center">
-        <div className="flex content-between items-center">
-          <h1 className="text-4xl font-bold tracking-[0.5em] text-white">
-            TODO
-          </h1>
-          <p onClick={e => toggleTheme(e)}>ICON</p>
-        </div>
+        <section className="mt-12 flex w-full max-w-xl px-6">
+          <div className="flex w-full justify-between">
+            <h1 className="text-2xl font-bold tracking-[0.5em] text-white md:text-4xl">
+              TODO
+            </h1>
+            <button onClick={e => toggleTheme(e)}>ICON</button>
+          </div>
+        </section>
       </main>
     </ThemeContext.Provider>
   );
