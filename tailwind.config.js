4a3bb0c9718927face64e88cfc55f3fb56fe5fd4
scottmotion <scott@scottmotion.com>
2023-08-31 09:59:56 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {},
+    colors: {
+      // primary
+      'bright-blue': 'hsl(220, 98%, 61%)',
+      'check-background': 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+      // light
+      'very-light-gray': 'hsl(0, 0%, 98%)',
+      'very-light-gray-blue': 'hsl(236, 33%, 92%)',
+      'light-gray-blue': 'hsl(233, 11%, 84%)',
+      'dark-gray-blue': 'hsl(236, 9%, 61%)',
+      'very-dark-gray-blue': 'hsl(235, 19%, 35%)',
+      // dark
+      'very-dark-blue': 'hsl(235, 21%, 11%)',
+      'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+      'light-gray-blue': 'hsl(234, 39%, 85%)',
+      'light-gray-blue-hover': 'hsl(236, 33%, 92%)',
+      'dark-gray-blue': 'hsl(234, 11%, 52%)',
+      'very-dark-gray-blue': 'hsl(233, 14%, 35%)',
+      'very-dark-gray-blue': 'hsl(237, 14%, 26%)',
+    },
+    fontFamily: {
+      sans: ['Josefin Sans', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
 };
+
