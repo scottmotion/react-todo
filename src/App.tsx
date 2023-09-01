c246ac1525f10bc92dcd1dcf840e4dd652e8d583
scottmotion <scott@scottmotion.com>
2023-09-01 06:40:00 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { createContext, useState, useEffect, MouseEvent } from 'react';
 import AppHeader from './Components/AppHeader';
+import NewTodo from './Components/NewTodo';
+
 export const ThemeContext = createContext('light');
+
+const myTodos = [{ title: 'Hello' }, { title: 'Hi' }];
 
 function App() {
   const [theme, setTheme] = useState('light');
+  const [todos, setTodos] = useState(myTodos);
 
   useEffect(() => {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -12,7 +17,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    var htmlElement = document.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
+    var htmlElement = document.getElementsByTagName('html')[0];
     if (theme === 'dark') {
       htmlElement.classList.add('dark');
     } else {
@@ -27,14 +32,14 @@ function App() {
     } else if (theme === 'light') {
       setTheme('dark');
     }
-    console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
   }
 
   return (
     <ThemeContext.Provider value={theme}>
       <main className="flex flex-col items-center">
-        <section className="mt-12 flex w-full max-w-xl px-6">
-          <AppHeader toggleTheme={toggleTheme} />{' '}
+        <section className="mt-20 flex w-full max-w-xl flex-col gap-y-6 px-6">
+          <AppHeader toggleTheme={toggleTheme} />
+          <NewTodo title={'Hello'} />
         </section>
       </main>
     </ThemeContext.Provider>
