c9d6942bcec4d845c0fca4ab4c58e5624022693c
scottmotion <scott@scottmotion.com>
2023-09-01 19:00:44 -0700

    Update TodoFilters.tsx

--- src/Components/TodoFilters.tsx
+++ src/Components/TodoFilters.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 export default function TodoFilters() {
+  const filters = ['All', 'Active', 'Completed'];
+  const [activeFilter, setActiveFilter] = useState(0);
   return (
     <div className="flex w-full items-center justify-center gap-6 rounded-md bg-white p-3 dark:bg-very-dark-desaturated-blue dark:text-white">
-      <p>All</p>
+      {/* <p>All</p>
       <p>Active</p>
-      <p>Completed</p>
+      <p>Completed</p> */}
+      {filters.map((filter, index) => (
+        <button
+          className={`${index === activeFilter ? 'text-bright-blue' : ''}`}
+          key={index}
+          onClick={() => setActiveFilter(index)}
+        >
+          {filter}
+        </button>
+      ))}
     </div>
   );
 }
