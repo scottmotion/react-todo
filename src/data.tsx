9fa3ba5cc79759f85bd2030109bc1031a9cc94a6
scottmotion <scott@scottmotion.com>
2023-09-14 10:55:14 -0700

    new data

--- src/data.tsx
+++ src/data.tsx
@@ -3,32 +3,32 @@ import { TodoType } from './Types/TodoType';
 export const todoData: TodoType[] = [
   {
     id: 'd35f6940-96d0-42ab-b5b0-cf8b7de56b17',
-    title: 'Todo 1',
+    title: 'Complete online JavaScript course',
     isCompleted: true,
   },
   {
     id: '8b7f3dde-6121-4480-9a2a-10a6476776c1',
-    title: 'Todo 2',
+    title: 'Jog around the park 3x',
     isCompleted: false,
   },
   {
     id: '71050f01-337e-4d5a-bfb2-1a08529de3d0',
-    title: 'Todo 3',
+    title: '10 minute meditation',
     isCompleted: false,
   },
   {
     id: '09393d0d-9b67-4579-a6ca-1c15139e2eae',
-    title: 'Todo 4',
+    title: 'Read for 1 hour',
     isCompleted: false,
   },
   {
     id: 'fcb13586-4dfa-4b6e-bdbe-0812631d868b',
-    title: 'Todo 5',
+    title: 'Pick up groceries',
     isCompleted: false,
   },
   {
     id: 'c56ac263-daf9-4168-9654-2d65e4138a96',
-    title: 'Todo 6',
+    title: 'Complete Todo App on Frontend Mentor',
     isCompleted: false,
   },
 ];
