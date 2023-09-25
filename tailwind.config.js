b5dc88bf2e36f21f96e3f61d9a6981b81a404cae
scottmotion <scott@scottmotion.com>
2023-09-25 15:59:36 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,24 +14,24 @@ export default {
       },
       colors: {
         // primary
-        'bright-blue': 'hsl(220, 98%, 61%)', // active text
-        'check-bg-start': 'hsl(192, 100%, 67%)', // gradient start
-        'check-bg-end': 'hsl(280, 87%, 65%)', // gradient end
+        'bright-blue': 'hsl(220, 98%, 61%)', // active filter text
+        'check-bg-start': 'hsl(192, 100%, 67%)', // check bg gradient start
+        'check-bg-end': 'hsl(280, 87%, 65%)', // check bg gradient end
 
         // light mode
         'very-light-gray': 'hsl(0, 0%, 98%)', // body bg
         'very-light-gray-blue': 'hsl(236, 33%, 92%)', // light mode borders
-        'light-gray-blue': 'hsl(233, 11%, 84%)',
-        'dark-gray-blue': 'hsl(236, 9%, 61%)',
-        'very-dark-gray-blue': 'hsl(235, 19%, 35%)',
+        'light-gray-blue': 'hsl(233, 11%, 84%)', // completed todo text
+        'dark-gray-blue': 'hsl(236, 9%, 61%)', // inactive grey text, input placeholder
+        'very-dark-gray-blue': 'hsl(235, 19%, 35%)', // todo text
 
         // dark mode
         'very-dark-blue': 'hsl(235, 21%, 11%)', // body bg
         'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)', //todo list bg
-        'light-gray-blue': 'hsl(234, 39%, 85%)',
-        'light-gray-blue-hover': 'hsl(236, 33%, 92%)',
+        'light-gray-blue': 'hsl(234, 39%, 85%)', // todo text
+        'light-gray-blue-hover': 'hsl(236, 33%, 92%)', // hover filter text
         'dark-gray-blue': 'hsl(234, 11%, 52%)',
-        'very-dark-gray-blue': 'hsl(233, 14%, 35%)',
+        'very-dark-gray-blue': 'hsl(233, 14%, 35%)', // completed todo text
         'very-dark-gray-blue-2': 'hsl(237, 14%, 26%)', // dark mode borders
       },
     },
