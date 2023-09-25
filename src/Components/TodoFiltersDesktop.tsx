8fc95b4d3a33a4a84f59ad54971137aae7ec8666
scottmotion <scott@scottmotion.com>
2023-09-25 16:55:42 -0700

    Update TodoFiltersDesktop.tsx

--- src/Components/TodoFiltersDesktop.tsx
+++ src/Components/TodoFiltersDesktop.tsx
@@ -10,15 +10,13 @@ export default function TodoFiltersDesktop({
   setActiveFilter,
 }: TodoFiltersProps) {
   return (
-    <div className="hidden w-auto items-center justify-center gap-6 rounded-md bg-white dark:bg-very-dark-desaturated-blue dark:text-white md:flex">
+    <div className="hidden w-auto items-center justify-center gap-6 rounded-md bg-white text-dark-gray-blue dark:bg-very-dark-desaturated-blue dark:text-white md:flex">
       <>
         {filters.map((filter, index) => (
           <button
             className={`${
-              filter === activeFilter
-                ? 'text-bright-blue opacity-100'
-                : 'opacity-50 hover:opacity-100'
-            } text-base `}
+              filter === activeFilter ? 'text-bright-blue' : 'hover:opacity-100'
+            } text-base hover:text-very-dark-gray-blue`}
             key={index}
             onClick={() => setActiveFilter(filter)}
           >
